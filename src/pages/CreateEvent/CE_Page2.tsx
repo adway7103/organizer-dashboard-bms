bb571b4e53b7cf6af7813d1f80a481471258e641
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-07-08 10:10:14 +0530

    new page created

---
+++ src/pages/CreateEvent/CE_Page2.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import { IoTicketOutline } from "react-icons/io5";
+const CE_Page2 = () => {
+  return (
+    <form>
+      <div className="flex items-center justify-between pb-8 p-1">
+        <span className="page-top bg-black"></span>
+        <span className="page-top bg-black"></span>
+      </div>
+      <h1 className="font-semibold text-2xl pb-4">Create Event</h1>
+      <h3 className="font-semibold text-lg">Tickets</h3>
+      <button className="add-ticket-btn font-light text-sm">
+        <Link to={"/ind-tickets"} className="add-ticket-btn">Add a Ticket</Link>
+      </button>
+      <p className="font-light text-sm pt-2">You can add tickets later from Tickets Menu</p>
+
+      <div className="tickets-display flex flex-col items-center justify-center my-5">
+            <IoTicketOutline className="text-9xl opacity-20"/>
+            <p className="font-light pt-2">You don't seem to have any bookings</p>
+            {/* <div className="all-tickets"></div> */}
+      </div>
+
+      
+
+      <div className="flex gap-x-8">
+        <button type="submit" className="event-form-btn">
+          SAVE CHANGES
+        </button>
+        <Link
+          to={"/create-events/2"}
+          className="event-form-btn bg-black text-white"
+        >
+          CREATE THIS EVENT
+        </Link>
+      </div>
+    </form>
+  )
+}
+
+export default CE_Page2;
--- src/pages/CreateEvent/CreateEvent.tsx
+++ src/pages/CreateEvent/CreateEvent.tsx
@@ -1,15 +1,9 @@
 import "./CreateEvent.css";
-import EventForm from "./EventForm";
 import { Outlet } from "react-router-dom";
 
 const CreateEvent = () => {
   return (
-    <section className="create-event w-1/2 px-10 pb-10  max-h-screen">
-      <div className="flex items-center justify-between pb-8 p-1">
-        <span className="page-top bg-black"></span>
-        <span className="page-top bg-neutral-300"></span>
-      </div>
-      <h1 className="font-semibold text-2xl pb-4">Create Event</h1>
+    <section className="create-event md:w-1/2 w-full md:px-0 px-10 mx-auto pb-10">
       <Outlet />
     </section>
   );
