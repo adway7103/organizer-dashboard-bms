200255eaeb74921af37a105ef129f10ac0cedff5
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-07-12 12:33:40 +0530

    minor changes

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,15 +3,7 @@ import SidebarNav from "./SidebarNav";
 import { SidebarNavs } from "../../utils/Constant";
 const Sidebar = () => {
   return (
-    // <div className="sidebar px-4 py-5 flex flex-col gap-y-1 gap-3">
-    //     {
-    //         SidebarNavs.map((item, index) => {
-    //             return (
-    //               <SidebarNav item={item} key={index} />
-    //             )
-    //         })
-    //     }
-    <div className="sidebar px-8 py-5 flex flex-col gap-y-4 gap-3">
+    <div className="sidebar px-8 py-5 flex flex-col gap-y-2 gap-3">
       {SidebarNavs.map((item, index) => {
         return <SidebarNav item={item} key={index} />;
       })}
