414ea9e6c5a9ec0b29fa27f48690a47662381ef0
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-07-18 01:11:00 +0530

    fix: add key

--- src/components/dashboard/Home/Affilliates.tsx
+++ src/components/dashboard/Home/Affilliates.tsx
@@ -30,21 +30,26 @@ const Data = [
 const Affilliates = () => {
   return (
     <div className="grid grid-cols-2 gap-4">
-      {Data.map((item) => (
-        <HomeContainerCard  className="border border-gray-300 rounded-2xl shadow-md  h-[27vh]">
-            <div className="flex pt-4 justify-center items-center">
-              <div className="h-10 w-10 rounded-full bg-yellow-500"></div>
-              <p className="pl-6">{item.title}</p>
+      {Data.map((item, index) => (
+        <HomeContainerCard
+          key={index}
+          className="border border-gray-300 rounded-2xl shadow-md  h-[27vh]"
+        >
+          <div className="flex pt-4 justify-center items-center">
+            <div className="h-10 w-10 rounded-full bg-yellow-500"></div>
+            <p className="pl-6">{item.title}</p>
+          </div>
+          <div className="flex justify-center items-center text-[1.8rem] font-bold">
+            {item.count}
+          </div>
+          <div className="flex xl:p-2 justify-center items-center">
+            <div className="text-xs sm:text-sm md:text-[0.7rem] lg:text-[0.7rem] px-1 py-1 bg-green-200 rounded-lg text-green-600">
+              +{item.percentage}
             </div>
-            <div className="flex justify-center items-center text-[1.8rem] font-bold">
-              {item.count}
-            </div>
-            <div className="flex xl:p-2 justify-center items-center">
-              <div className="text-xs sm:text-sm md:text-[0.7rem] lg:text-[0.7rem] px-1 py-1 bg-green-200 rounded-lg text-green-600">
-                +{item.percentage}
-              </div>
-              <p className="text-xs md:text-[0.8rem] lg:text-xs pl-3">{item.days}</p>
-            </div>
+            <p className="text-xs md:text-[0.8rem] lg:text-xs pl-3">
+              {item.days}
+            </p>
+          </div>
         </HomeContainerCard>
       ))}
     </div>
