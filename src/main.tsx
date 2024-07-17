7e96d8edba98f2ec0a9e204792c31d02548bb1d5
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-07-18 01:29:15 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from "./utils/router.jsx";
 import './index.css';
 import { EventProvider } from './Contexts/CreateEventContext.tsx';
+import { AuthProvider } from './Contexts/AuthContext.tsx';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-     <EventProvider>
-        <RouterProvider router={router} />
-     </EventProvider>
-  </React.StrictMode>,
+   <React.StrictMode>
+    <AuthProvider>
+      <EventProvider>
+        <RouterProvider router={router}>
+        </RouterProvider>
+      </EventProvider>
+    </AuthProvider>
+  </React.StrictMode>
 );
