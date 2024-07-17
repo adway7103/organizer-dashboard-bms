3aa7152a3375af27a562687e2be1178030d5a7f9
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-07-18 01:12:24 +0530

    add pie chart

--- src/components/dashboard/Affiliates/Total.tsx
+++ src/components/dashboard/Affiliates/Total.tsx
@@ -1,4 +1,5 @@
 import HomeContainerCard from "../HomeContainerCard";
+import AffiliatePieChart from "./AffiliatePieChart";
 
 interface PropsType {
   heading: string;
@@ -7,8 +8,8 @@ const Total = ({ heading }: PropsType) => {
   return (
     <HomeContainerCard className="h-auto md:h-[25vh] border border-gray-300 rounded-3xl">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="place-self-center">pie chart</div>
-        <div className="flex flex-col justify-center items-center ">
+        <div className="place-self-center mt-2 md:ml-0 md:pl-0 md:mt-6 md:ml-10"><AffiliatePieChart/></div>
+        <div className="flex flex-col items-center md:mt-7">
           <div className="p-2 xl:p-4 font-semibold text-base md:text-xl">{heading}</div>
           <div className="text-xs md:text-sm">
             <div className="flex p-1 gap-4 items-center justify-between">
