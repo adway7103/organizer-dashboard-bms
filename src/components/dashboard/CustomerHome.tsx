64de983685edf194034a20e6de7cdd7786f86b21
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-07-27 01:46:33 +0530

    fix responsiveness

--- src/components/dashboard/CustomerHome.tsx
+++ src/components/dashboard/CustomerHome.tsx
@@ -6,23 +6,22 @@ import FollowerPieChart from "./Followers/FollowerPieChart";
 
 const CustomerHome = () => {
   return (
-    <div className="ml-8">
-      <h1 className="font-semibold text-[2rem] pl-4">Customers</h1>
-
-      <div className="grid grid-cols-5 gap-4 my-3">
-        <div className="col-span-1">
+    <div className="sm:ml-8 min-w-[300px]">
+      <h2 className="font-semibold text-[2rem] px-4">Customers</h2>
+      <div className="grid grid-cols-6 xl:grid-cols-10 gap-4 my-3">
+        <div className="col-span-6 md:col-span-2 xl:col-span-2">
           <div>
-            <CountCard heading={"Customers"} count={"5000"} />
+            <CountCard heading={"Followers"} count={"5000"}/>
           </div>
           <div>
             <TextBlast />
           </div>
         </div>
-        <div className="col-span-2">
+        <div className="col-span-6 md:col-span-4 xl:col-span-4">
           <CustomerChart />
         </div>
-        <div className="col-span-1"><FollowerPieChart heading={"Attendees By Gender"}/></div>
-        <div className="col-span-1"><FollowerPieChart heading={"Attendees By Age"}/></div>
+        <div className="col-span-6 md:col-span-3 xl:col-span-2"><FollowerPieChart heading={"Attendees By Gender"}/></div>
+        <div className="col-span-6 md:col-span-3 xl:col-span-2"><FollowerPieChart heading={"Attendees By Age"}/></div>
       </div>
       <CustomerTable />
     </div>
@@ -30,3 +29,4 @@ const CustomerHome = () => {
 };
 
 export default CustomerHome;
+
