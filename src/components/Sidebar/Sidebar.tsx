bdefa9a789b863013261ea81275c15cd287f11e0
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-07-27 13:29:55 +0530

    extend sidebar length

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import { SidebarNavs } from "../../utils/Constant";
 const Sidebar = ({ isVisible }: any) => {
   return (
     <div
-      className={`sidebar py-5 md:px-8 md:py-5 flex flex-col space-y-2 transition-transform duration-300 ease-in-out ${
+      className={`sidebar h-[90%] py-5 md:px-8 md:py-5 flex flex-col space-y-2 transition-transform duration-300 ease-in-out ${
         isVisible || window.innerWidth >= 768
           ? "translate-x-0"
           : "-translate-x-full"
