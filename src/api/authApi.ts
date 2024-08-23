18b031f63c6fc878c9e2e2b160764246e7900ee1
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-08-24 01:39:07 +0530

    fix

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -6,6 +6,7 @@ type UserSignupDataType = {
   gender: string;
   phone: string;
   email: string;
+  fcmToken: string;
   password: string;
   isTnCAccepted: boolean;
   isPrivacyPolicyAccepted: boolean;
