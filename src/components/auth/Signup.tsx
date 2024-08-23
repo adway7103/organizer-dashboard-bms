78304cad1b61e8132e8996adf08c9619afb1b076
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-08-24 01:40:56 +0530

    sign up api

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Loader2 } from "lucide-react";
 import { MuiTelInput } from "mui-tel-input";
 import { useSignUp } from "../../api/authApi";
+import { useAuth } from "../../Contexts/AuthContext";
 import toast from "react-hot-toast";
 
 const Register = () => {
@@ -11,10 +12,10 @@ const Register = () => {
   const [lastName, setLastName] = useState(""); // State for last name input
   const [gender, setGender] = useState("male"); // State for gender
   const [phone, setPhone] = useState("");
-  const [error, setError] = useState<string | null>(null);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const { login } = useAuth();
 
   const navigate = useNavigate();
   const handleInputChange = (event: any) => {
@@ -45,45 +46,27 @@ const Register = () => {
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
-    const missingFields = [];
-    if (!firstName) missingFields.push("First Name");
-    if (!lastName) missingFields.push("Last Name");
-    if (!phone) missingFields.push("Phone");
-    if (!email) missingFields.push("Email");
-    if (!password) missingFields.push("Password");
-
-    if (missingFields.length > 0) {
-      if (missingFields.length === 5) {
-        setError("All fields are required");
-      } else {
-        setError(
-          `${missingFields.join(", ")} ${
-            missingFields.length > 1 ? "are" : "is"
-          } required`
-        );
-      }
-      return;
-    }
 
     try {
       setLoading(true);
-      await useSignUp({
+      const response = await useSignUp({
         email,
         password,
         fname: firstName,
         lname: lastName,
         gender,
+        fcmToken: "test token",
         phone,
         isTnCAccepted: true,
         isPrivacyPolicyAccepted: true,
       });
-      navigate("/login");
+      const token = response.data.accessToken;
+      login(token);
       toast.success("Sign up successfull");
+      navigate("/createanaccount");
     } catch (error: any) {
       if (error.response.status === 409) {
-        setError(
-          "Email or phone number already in use. Please try again with a different one."
-        );
+        toast.error("Email or phone number already in use.");
       } else {
         console.error("signup error:", error);
       }
@@ -94,20 +77,29 @@ const Register = () => {
 
   return (
     <>
-      <div className="md:flex">
-        <div className="md:w-[46vw] md:block hidden">
-          <img src="/OrganizerLogin.png" className="h-screen w-full" alt="" />
+      <div className="flex h-screen ">
+        <div className="md:w-1/2 md:block hidden">
+          <img
+            src="/OrganizerLogin.png"
+            className="h-full w-full object-cover"
+            alt=""
+          />
         </div>
-        <div className="h-screen">
-          <div className="py-[7vh] 2xl:py-[12vh] px-[10vw] h-full md:w-[54vw]">
+        <div className="h-screen w-full md:w-1/2 flex justify-center items-center">
+          <div className="md:px-[10vw]">
             <form
               onSubmit={handleSubmit}
-              className="h-full p-4 flex flex-col justify-around"
+              className="h-full p-4 w-full space-y-3"
             >
-              <div className="flex flex-col gap-2">
-                <h1 className="text-3xl font-bold text-center items-center">
-                  Sign Up
-                </h1>
+              <div className="flex flex-col items-center">
+                {/* <Link to={"/"}>
+                  <img
+                    src="/logo-nobg.png"
+                    alt="logo"
+                    className="max-h-[10vh] max-w-[40vw]"
+                  />
+                </Link> */}
+                <h1 className="text-3xl font-bold text-center">Sign Up</h1>
               </div>
 
               <div className="w-full">
@@ -115,8 +107,9 @@ const Register = () => {
                   type="text"
                   name="firstName"
                   id="firstName"
+                  required={true}
                   value={firstName}
-                  placeholder="First name"
+                  placeholder="First name *"
                   className="w-full"
                   onChange={handleInputChange}
                 />
@@ -126,7 +119,8 @@ const Register = () => {
                   type="text"
                   name="lastName"
                   id="lastName"
-                  placeholder="Last name"
+                  required={true}
+                  placeholder="Last name *"
                   value={lastName}
                   className="w-full"
                   onChange={handleInputChange}
@@ -139,6 +133,7 @@ const Register = () => {
                     type="radio"
                     name="gender"
                     value="male"
+                    required={true}
                     checked={gender === "male"}
                     onChange={handleInputChange}
                   />
@@ -149,6 +144,7 @@ const Register = () => {
                     type="radio"
                     name="gender"
                     value="female"
+                    required={true}
                     checked={gender === "female"}
                     onChange={handleInputChange}
                   />
@@ -173,8 +169,9 @@ const Register = () => {
                 <input
                   type="email"
                   name="email"
+                  required={true}
                   value={email}
-                  placeholder="Email Address"
+                  placeholder="Email Address *"
                   id="email"
                   className="w-full"
                   onChange={handleInputChange}
@@ -185,7 +182,8 @@ const Register = () => {
                 <input
                   type="password"
                   value={password}
-                  placeholder="Password"
+                  required={true}
+                  placeholder="Password *"
                   name="password"
                   id="password"
                   className="w-full"
@@ -207,17 +205,24 @@ const Register = () => {
                 <div className="h-[2px] bg-black"></div>
                 <p className="text-center text-sm">
                   By registering, you agree to KAFSCO's{" "}
-                  <a href="/terms">T&Cs</a> and
-                  <a href="/privacy">Privacy Policy.</a>
+                  <a
+                    target="blank"
+                    href="/terms"
+                    className="text-blue-600 font-bold"
+                  >
+                    T&Cs
+                  </a>{" "}
+                  and{" "}
+                  <a href="/privacy" className="text-blue-600 font-bold">
+                    Privacy Policy.
+                  </a>
                 </p>
-                <div>
-                  <div className="text-red-500">{error}</div>
-                  <div className="flex justify-center gap-1">
-                    Already an organizer?
-                    <Link to="/login" className="text-[#8C3E87] pl-2">
-                      Login
-                    </Link>
-                  </div>
+                <div className="flex justify-center items-center font-semibold gap-x-2">
+                  {/* <div className="text-red-500">{error}</div> */}
+                  Already a member ?{" "}
+                  <Link to="/login" className="text-[#8C3E87]">
+                    Login
+                  </Link>
                 </div>
               </div>
             </form>
