1d438ac69b2bcedb05c691e853a74fc3f1b01351
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-08-24 01:35:19 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,16 @@ import "./index.css";
 import { EventProvider } from "./Contexts/CreateEventContext.tsx";
 import { AuthProvider } from "./Contexts/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
+import { AccountProvider } from "./Contexts/createAnAccountContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <EventProvider>
-        <RouterProvider router={router}></RouterProvider>
-        <Toaster position="bottom-center" reverseOrder={false} />
+        <AccountProvider>
+          <RouterProvider router={router}></RouterProvider>
+          <Toaster position="top-center" reverseOrder={false} />
+        </AccountProvider>
       </EventProvider>
     </AuthProvider>
   </React.StrictMode>
