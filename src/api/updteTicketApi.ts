9a4c652c1666e4aae1f6188abe4a3746d78c5a39
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-09-01 01:36:11 +0530

    update ticket api integrated

---
+++ src/api/updteTicketApi.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const updateTicket = async ({ data, ticketId, matrixId }: any) => {
+  console.log("data",data);
+  console.log("ticketId",ticketId);
+  console.log("matrixId",matrixId);
+
+  const token = localStorage.getItem("accessToken");
+
+  try {
+    const response = await axios.put(
+      `https://kafsbackend-106f.onrender.com/api/v1/bookingmatrix/update/${matrixId}`,
+      { ticketId, updatedData: data },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
