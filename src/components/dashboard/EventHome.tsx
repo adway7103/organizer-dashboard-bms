41d7c1c6cafb6e9b0df3cce3fa9ebeed3886f732
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-09-08 15:12:06 +0530

    conditional changes

--- src/components/dashboard/EventHome.tsx
+++ src/components/dashboard/EventHome.tsx
@@ -1,10 +1,20 @@
+import { useLocation } from "react-router-dom";
 import EventGrid from "./Events/EventGrid";
 import Total from "./Events/Total";
 
 export const EventHome = () => {
+  const location = useLocation();
+
+  const isLiveEvents = location.pathname.includes("/live-events");
+  const isPastEvents = location.pathname.includes("/past-events");
+
   return (
     <div className="sm:ml-8 sm:mr-16 min-w-[300px]">
-      <h1 className="text-3xl font-semibold">Live Events</h1>
+      <h1 className="text-3xl font-semibold pl-4">
+        {isLiveEvents && "Live Events"}
+        {isPastEvents && "Past Events"}
+        
+      </h1>
       <div className="flex flex-col md:flex-row lg:w-auto xl:w-auto gap-6 mt-6">
         <Total heading={"Total Revenue"} />
         <Total heading={"Total tickets sold"} />
