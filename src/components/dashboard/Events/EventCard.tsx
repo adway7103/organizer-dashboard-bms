1be90837c49deb0a9e0b47cfd0f349f14efb1b92
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-09-09 23:32:34 +0530

    added icon

--- src/components/dashboard/Events/EventCard.tsx
+++ src/components/dashboard/Events/EventCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import img from "../../../../public/traffic.png"
 
 interface EventCardProps {
   eventId: string;
@@ -64,7 +65,7 @@ const EventCard = ({
       </div>
       <div>
         <div className="flex justify-center sm:justify-between items-center">
-          <h1 className="text-3xl font-semibold text-[#a28a02] text-center md:text-start">
+          <h1 className="text-2xl font-semibold text-[#a28a02] text-center md:text-start">
             {title}
           </h1>
           <svg
@@ -160,20 +161,7 @@ const EventCard = ({
             <div className="flex gap-1">
               Traffic
               <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1"
-                  stroke="currentColor"
-                  className="size-5"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                  />
-                </svg>
+            <img src={img} alt="" className="flex items-center size-4"/>
               </div>
             </div>
           </div>
@@ -201,7 +189,7 @@ const EventCard = ({
         </div>
         <div className="flex text-white text-lg space-x-2 mt-3">
           <div className="border bg-[#000000] px-8 py-1">Preview</div>
-          <div className="border bg-[#954b7c] px-8 py-1">Text Blast</div>
+          <div className="border bg-[#954b7c] px-7 sm:px-8 py-1">Text Blast</div>
         </div>
         <div className="flex justify-between border-t mt-3 pt-1 text-sm">
           <p>Revenue</p>
