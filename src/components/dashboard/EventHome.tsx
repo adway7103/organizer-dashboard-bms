125e9ec4a1744d663a6cf8c62d484ead7ea1e13b
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-09-12 01:01:14 +0530

    live and past event api integrated

--- src/components/dashboard/EventHome.tsx
+++ src/components/dashboard/EventHome.tsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import EventGrid from "./Events/EventGrid";
 import Total from "./Events/Total";
+import { fetchPastEvents } from "../../api/fetchPastEvents";
+import { fetchLiveEvents } from "../../api/fetchLiveEvents";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+interface EventData {
+  eventId: string;
+  title: string;
+  posterUrl: string;
+  city: string;
+  date: string;
+  time: string;
+  revenue: string;
+  ticketsSold: string;
+}
 
 export const EventHome = () => {
   const location = useLocation();
@@ -8,18 +24,71 @@ export const EventHome = () => {
   const isLiveEvents = location.pathname.includes("/live-events");
   const isPastEvents = location.pathname.includes("/past-events");
 
+  const [events, setEvents] = useState<EventData[]>([]);
+
+  const fetchData = async () => {
+    try {
+      let response;
+      if (isLiveEvents) {
+        response = await fetchLiveEvents();
+      } else if (isPastEvents) {
+        response = await fetchPastEvents();
+      }
+
+      const formattedEvents = response.events.map((event: EventData) => ({
+        eventId: event.eventId,
+        title: event.title,
+        posterUrl: event.posterUrl,
+        city: event.city,
+        date: event.date,
+        time: event.time,
+        revenue: event.revenue,
+        ticketsSold: event.ticketsSold,
+      }));
+
+      setEvents(formattedEvents);
+    } catch (error) {
+      console.error("Failed to fetch events", error);
+      // toast.error("Failed to fetch events");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [location.pathname]);
+
+  const handleDelete = async (eventId: string) => {
+    const token = localStorage.getItem("accessToken");
+
+    try {
+      await axios.delete(
+        `https://kafsbackend-106f.onrender.com/api/v1/events/delete/${eventId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      toast.success("Event Deleted successfully!");
+      fetchData();
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to delete event");
+    }
+  };
+
   return (
     <div className="sm:ml-8 sm:mr-16 min-w-[300px]">
       <h1 className="text-3xl font-semibold pl-4">
         {isLiveEvents && "Live Events"}
         {isPastEvents && "Past Events"}
-        
       </h1>
       <div className="flex flex-col md:flex-row lg:w-auto xl:w-auto gap-6 mt-6">
         <Total heading={"Total Revenue"} />
         <Total heading={"Total tickets sold"} />
       </div>
-      <EventGrid />
+
+      <EventGrid events={events} handleDelete={handleDelete} />
     </div>
   );
 };
--- src/components/dashboard/Events/EventGrid.tsx
+++ src/components/dashboard/Events/EventGrid.tsx
@@ -1,11 +1,8 @@
-import { Search } from "lucide-react";
-import EventCard from "../Events/EventCard";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { MenuItem, Select } from "@mui/material";
+import { Search } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import { fetchEvents } from "../../../api/fetchAllEvents";
-import axios from "axios";
-import toast from "react-hot-toast";
+import EventCard from "../Events/EventCard";
 
 interface EventData {
   eventId: string;
@@ -18,59 +15,18 @@ interface EventData {
   ticketsSold: string;
 }
 
-const EventGrid = () => {
+interface EventGridProps {
+  events: EventData[];
+  handleDelete: (eventId: string) => void;
+}
+
+const EventGrid = ({ events, handleDelete }: EventGridProps) => {
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
-
-  const [events, setEvents] = useState<EventData[]>([]);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetchEvents();
-      const formattedEvents = response.events.map((event: EventData) => ({
-        eventId: event.eventId,
-        title: event.title,
-        posterUrl: event.posterUrl,
-        city: event.city,
-        date: event.date,
-        time: event.time,
-        revenue: event.revenue,
-        ticketsSold: event.ticketsSold,
-      }));
-      setEvents(formattedEvents);
-    } catch (error) {
-      console.error("Failed to fetch events", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   const handleEventClick = (eventId: string) => {
     navigate(`event-overview/${eventId}`);
   };
-
-  const handleDelete = async (eventId: string) => {
-    const token = localStorage.getItem("accessToken");
-    
-    try {
-      await axios.delete(
-        `https://kafsbackend-106f.onrender.com/api/v1/events/delete/${eventId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      toast.success("Event Deleted successfully!");
-      fetchData();
-    } catch (error) {
-      console.log(error);
-      toast.error("Failed to delete event");
-    }
-  };
-  
 
   return (
     <div>
@@ -90,7 +46,6 @@ const EventGrid = () => {
           <Select
             defaultValue={10}
             size="small"
-            className=""
             sx={{
               backgroundColor: "#E6E6E682",
               paddingX: 2,
@@ -106,19 +61,19 @@ const EventGrid = () => {
       </div>
 
       <div className="flex flex-wrap gap-6 mt-6">
-        {events.map((i, index) => (
+        {events.map((event, index) => (
           <EventCard
             key={index}
-            eventId={i.eventId}
-            title={i.title}
-            posterUrl={i.posterUrl}
-            city={i.city}
-            time={i.time}
-            date={i.date}
-            revenue={i.revenue}
-            ticketsSold={i.ticketsSold}
-            onClick={() => handleEventClick(i.eventId)}
-            handleDelete={() => handleDelete(i.eventId)}
+            eventId={event.eventId}
+            title={event.title}
+            posterUrl={event.posterUrl}
+            city={event.city}
+            time={event.time}
+            date={event.date}
+            revenue={event.revenue}
+            ticketsSold={event.ticketsSold}
+            onClick={() => handleEventClick(event.eventId)}
+            handleDelete={() => handleDelete(event.eventId)}
           />
         ))}
       </div>
