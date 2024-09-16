8a1b93ddfbd228bbaba757f6f50eebd2e391e79c
Pratik <pratikkhatri520@gmail.com>
2024-09-16 03:24:42 -0700

    change button name

--- src/pages/CreateEvent/CE_Page2.tsx
+++ src/pages/CreateEvent/CE_Page2.tsx
@@ -243,7 +243,7 @@ const CE_Page2: React.FC = () => {
         </div> */}
         <Link to={"/drafted-events"}>
           <button className="flex flex-row items-center justify-center gap-4 bg-gray-100 text-black font-bold py-2 px-10 rounded">
-            CANCEL
+            SAVE AS DRAFT
           </button>
         </Link>
         <div>
