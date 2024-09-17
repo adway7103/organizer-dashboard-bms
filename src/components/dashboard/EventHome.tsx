97a43c65d0bbec09ff2c7647064746ddf5461044
Pratik <pratikkhatri520@gmail.com>
2024-09-17 02:57:25 -0700

    changes in drafted event page

--- src/components/dashboard/EventHome.tsx
+++ src/components/dashboard/EventHome.tsx
@@ -6,6 +6,8 @@ import { fetchPastEvents } from "../../api/fetchPastEvents";
 import { fetchLiveEvents } from "../../api/fetchLiveEvents";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { fetchDraftedEvents } from "../../api/fetchDraftedEvents";
+import { updateEvent } from "../../api/updateEvent";
 
 interface EventData {
   eventId: string;
@@ -24,6 +26,7 @@ export const EventHome = () => {
 
   const isLiveEvents = location.pathname.includes("/live-events");
   const isPastEvents = location.pathname.includes("/past-events");
+  const isDraftEvents = location.pathname.includes("/drafted-events");
 
   const [events, setEvents] = useState<EventData[]>([]);
 
@@ -34,6 +37,9 @@ export const EventHome = () => {
         response = await fetchLiveEvents();
       } else if (isPastEvents) {
         response = await fetchPastEvents();
+      } else {
+        response = await fetchDraftedEvents();
+        console.log(response);
       }
 
       const formattedEvents = response.events.map((event: EventData) => ({
@@ -79,18 +85,35 @@ export const EventHome = () => {
     }
   };
 
+  const handleTurnLiveButton = async (eventId: string) => {
+    try {
+      await updateEvent({ eventStatus: "Published" }, eventId);
+      toast.success("Event is Live");
+      fetchData();
+    } catch (error) {
+      console.error("Failed to update event", error);
+    }
+  };
+
   return (
     <div className="sm:ml-8 sm:mr-16 min-w-[300px]">
       <h1 className="text-3xl font-semibold pl-4">
         {isLiveEvents && "Live Events"}
         {isPastEvents && "Past Events"}
+        {isDraftEvents && "Drafted Events"}
       </h1>
-      <div className="flex flex-col md:flex-row lg:w-auto xl:w-auto gap-6 mt-6">
-        <Total heading={"Total Revenue"} />
-        <Total heading={"Total tickets sold"} />
-      </div>
+      {!isDraftEvents && (
+        <div className="flex flex-col md:flex-row lg:w-auto xl:w-auto gap-6 mt-6">
+          <Total heading={"Total Revenue"} />
+          <Total heading={"Total tickets sold"} />
+        </div>
+      )}
 
-      <EventGrid events={events} handleDelete={handleDelete} />
+      <EventGrid
+        events={events}
+        handleDelete={handleDelete}
+        handleTurnLiveButton={handleTurnLiveButton}
+      />
     </div>
   );
 };
