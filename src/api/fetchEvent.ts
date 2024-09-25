2b22c1c6360447cab960c8edaa026e9990392c09
Pratik <pratikkhatri520@gmail.com>
2024-09-25 15:56:36 +0530

    fix

--- src/api/fetchEvent.ts
+++ src/api/fetchEvent.ts
@@ -4,7 +4,7 @@ export const fetchEvent = async ({ eventId }: any) => {
   const token = localStorage.getItem("accessToken");
 
   const response = await axios.get(
-    `api.kafsco.com/api/v1/events/geteventbyid/${eventId}`,
+    `https://api.kafsco.com/api/v1/events/geteventbyid/${eventId}`,
     {
       headers: {
         Authorization: `Bearer ${token}`,
