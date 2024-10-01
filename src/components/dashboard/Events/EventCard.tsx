7d9830f0326aa54fb92c3a03572887721a62e645
Pratik <pratikkhatri520@gmail.com>
2024-10-02 00:52:10 +0530

    added track button

--- src/components/dashboard/Events/EventCard.tsx
+++ src/components/dashboard/Events/EventCard.tsx
@@ -81,6 +81,11 @@ const EventCard = ({
     e.stopPropagation();
     navigate(`event-overview/${eventId}`);
   };
+
+  const handleTrackLiveClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    // navigate(`event-overview/${eventId}`);
+  };
   const truncateTitle = (title: string, wordLimit: number) => {
     const words = title.split(" "); // Split the title into an array of words
     return words.length > wordLimit
@@ -214,7 +219,7 @@ const EventCard = ({
             </div>
           ) : (
             <div
-              className="border bg-[#ededed] px-4 py-1 rounded-full  hover:shadow-lg"
+              className="border bg-[#ededed] px-4 py-1 rounded-full hover:shadow-lg"
               onClick={handleTurnLiveClick}
             >
               <div className="flex gap-1">
@@ -238,27 +243,33 @@ const EventCard = ({
               </div>
             </div>
           )}
-          {/* <div className="border bg-[#ededed] px-4 py-1 rounded-full">
-            <div className="flex gap-1">
-              Track
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1"
-                  stroke="currentColor"
-                  className="size-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
-                  />
-                </svg>
+
+          {!isDraftEvents ? (
+            <div className="border bg-[#ededed] px-4 py-1 rounded-full hover:shadow-lg"               onClick={handleTrackLiveClick}
+>
+              <div className="flex gap-1">
+                Track
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1"
+                    stroke="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
-          </div> */}
+          ) : (
+            <div></div>
+          )}
         </div>
         <div
           className="flex text-white text-lg space-x-2 mt-3"
