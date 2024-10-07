846b79947d6055aed15b0b687578903d23950b93
Pratik <pratikkhatri520@gmail.com>
2024-10-07 17:00:01 +0530

    fix event table

--- src/components/dashboard/Home/TableRow.tsx
+++ src/components/dashboard/Home/TableRow.tsx
@@ -1,29 +1,25 @@
-// import img from '../../../../public/dashboard/image.png'
-const TableRow: React.FC<any> = ({
-  event,
-  img,
-  date,
-  venueAddress,
-  // totalTicket,
-  // amount,
-  //   stdout,
-}) => {
+const TableRow: React.FC<any> = ({ event, img, date, venueAddress }) => {
   return (
     <tr className=" border-b border-b-black">
-      <th scope="row" className="py-4 font-medium text-gray-900 flex gap-2">
-        <img
-          src={img}
-          alt="random"
-          className="size-12 object-cover rounded-full"
-        />
-        <div>
-          <h1 className="font-semibold">{event}</h1>
-          {/* <p className="text-xs text-gray-800">event info</p> */}
+      <th
+        scope="row"
+        className="py-4 items-center font-medium text-gray-900 gap-2"
+      >
+        <div className="flex justify-ccenter items-center gap-2">
+          <img
+            src={img}
+            alt="random"
+            className="size-12 object-cover rounded-full"
+          />
+          <h1 className="font-semibold hover:line-clamp-none line-clamp-2 leading-tight">
+            {event}
+          </h1>
         </div>
-        {/* {event} */}
       </th>
       <td className="px-6">{date}</td>
-      <td className="px-6">{venueAddress}</td>
+      <td className="px-6 pt-4 hover:line-clamp-none line-clamp-2">
+        {venueAddress}
+      </td>
     </tr>
   );
 };
