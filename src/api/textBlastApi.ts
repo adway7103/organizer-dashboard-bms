6bbd975f391b1919c882470f9c47f45f68bc83ce
Pratik <pratikkhatri520@gmail.com>
2024-11-13 19:27:20 +0530

    added text blast api

--- src/api/textBlastApi.ts
+++ src/api/textBlastApi.ts
@@ -1,15 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 import { baseUrl } from "../utils";
 
-export const textBlast = async (data: { subject: string; description: string }) => {
+export const textBlast = async (
+  data: {
+    subject: string;
+    description: string;
+  },
+  endPoint: string
+) => {
   const token = localStorage.getItem("accessToken");
 
   try {
     const response = await axios.post(
-      `https://${baseUrl}/api/v1/organizers/text-blast/email`,
+      `https://${baseUrl}/api/v1/organizers/email-blast/${endPoint}`,
       {
-        subject: data.subject, 
-        description: data.description, 
+        emailSubject: data.subject,
+        emailBody: data.description,
       },
       {
         headers: {
@@ -17,7 +23,38 @@ export const textBlast = async (data: { subject: string; description: string }) 
         },
       }
     );
-    return response
+    return response;
+  } catch (error) {
+    console.error("Error sending text blast:", error);
+    throw error;
+  }
+};
+
+export const EventTextBlast = async (
+  data: {
+    subject: string;
+    description: string;
+  },
+  eventId: any
+) => {
+  const token = localStorage.getItem("accessToken");
+  if (!eventId) return;
+  console.log(eventId);
+  
+  try {
+    const response = await axios.post(
+      `https://${baseUrl}/api/v1/organizers/email-blast/event/${eventId}`,
+      {
+        emailSubject: data.subject,
+        emailBody: data.description,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response;
   } catch (error) {
     console.error("Error sending text blast:", error);
     throw error;
